c04c6285c8cb7eb89e68bfb5895f5e09a9c26a87
jtx <jiaotianxiao@itengshe.com>
2024-09-05 11:27:50 +0800

    add practice 1-11 1-12

---
+++ practice/1-12.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+/**
+ * 练习 1-12 编写一个程序，以每行一个单词的形式打印其输入。
+ *  */
+void main()
+{
+    int c, nl, nw, nc, state;
+    state = OUT;
+    while ((c = getchar()) != EOF)
+    {
+        if (c == ' ' || c == '\n' || c == '\t') {
+            state = OUT;
+            putchar("\n");
+        } else if (state == OUT) {
+            state = IN;
+            putchar(c);
+        }
+    }
+}
